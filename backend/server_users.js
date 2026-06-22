84eddf3ebfd38167e9d6f2188c898bc38e0a7008
Erman <66209038+Xerman09@users.noreply.github.com>
2026-06-22 16:16:33 +0800

    update rc2 id layout

--- backend/server_users.js
+++ backend/server_users.js
@@ -1,5 +1,5 @@
 // backend/server_users.js
-require('dotenv').config();
+require('dotenv').config({ path: require('path').join(__dirname, '.env') });
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
